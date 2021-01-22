44748333925a4536f320a5ba45f0cb6854a505b0
Oisera <orinamark22@gmail.com>
2021-01-22 11:03:24 +0300

    added return statement

--- Methodsvsfunctions.py
+++ Methodsvsfunctions.py
@@ -2,16 +2,19 @@
 
 # Method
 class Ship:
-    def __init__(self, name, capacity):
+    def __init__(self, name, capacity, cargo):
         self.name = name
         self.capacity = capacity
-        self.cargo = 0
+        self.cargo = int(cargo)
 
     def sail(self):
         print("{} has sailed!".format(self.name))
 
     def cap(self):
         print("{} passengers".format(self.capacity))
+
+    def convert_cargo(self):
+        return self.cargo * 1000
 
 
 # function
@@ -25,10 +28,11 @@ def set_cap(capacity):
 
 # creating an instance of the class Ship
 # and calling the method sail
-black_pearl = Ship("Black Pearl", 800)
+black_pearl = Ship("Black Pearl", 800, 2)
 black_pearl.sail()
 black_pearl.cap()
-
+black_pearl.convert_cargo()
+print(black_pearl.convert_cargo())
 # calling the function sail_function
 sail_function(black_pearl.name)
 set_cap(black_pearl.capacity)
